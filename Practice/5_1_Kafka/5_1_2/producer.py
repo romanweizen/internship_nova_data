522074c1b687c31bc7d42648bf5003ad4291b1a4
romanweizen <dismember8877@gmail.com>
2025-11-20 18:45:25 +0300

    added folder kafka task + 5_1_3

--- Practice/5_1_Kafka/5_1_2/producer.py
+++ Practice/5_1_Kafka/5_1_2/producer.py
@@ -3,12 +3,15 @@ import json
 import time
 import random
 
+
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
+
 users = ["alice", "bob", "carol", "dave"]
+
 
 while True:
     data = {
