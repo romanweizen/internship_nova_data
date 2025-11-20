1152455f379e8e326080756e70280de67091bbc3
romanweizen <dismember8877@gmail.com>
2025-11-20 18:00:18 +0300

    added new excercise

--- Practice/5_1_Kafka/5_1_2/producer.py
+++ Practice/5_1_Kafka/5_1_2/producer.py
@@ -4,18 +4,18 @@ import time
 import random
 
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
 users = ["alice", "bob", "carol", "dave"]
 
 while True:
-    data = {
-        "user": random.choice(users),
-        "event": "login",
-        "timestamp": time.time()
-    }
-    producer.send("user_events", value=data)
-    print("Sent:", data)
-    time.sleep(1)
+    data = {
+        "user": random.choice(users),
+        "event": "login",
+        "timestamp": time.time()
+    }
+    producer.send("user_events", value=data)
+    print("Sent:", data)
+    time.sleep(1)
